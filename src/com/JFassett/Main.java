5750d08c9fd06590a2a53601a446a9bed5ef2270
creoix <jtfassett@gmail.com>
2018-07-01 13:01:08 -0600

    Add Project Settings that were ignored for some reason from with previous .gitignore

--- src/com/JFassett/Main.java
+++ src/com/JFassett/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static WebDriverWait wait;
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.setProperty("webdriver.gecko.driver", "drivers\\geckodriver.exe");
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 40);
